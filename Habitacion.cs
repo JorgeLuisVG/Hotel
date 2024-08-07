93324f83dd48a8339eec0e93029c6bba5339e566
JorgeLuisVG <158310235+JorgeLuisVG@users.noreply.github.com>
2024-08-07 09:07:17 -0600

    Se agregaron metodos

--- Habitacion.cs
+++ Habitacion.cs
@@ -3,8 +3,48 @@
     public class Habitacion
     {
         public int Numero { get; set; }
-    
-    
+        public double Costo { get; set; }
+        private string NombreCliente { get; set; }
+        public int NumeroCamas { get; set; }
+        public bool Disponibilidad { get; set; }
+
+        public Habitacion(int numero, double costo, string nombreCliente, int numeroCamas, bool disponibilidad)
+        { 
+            Numero = numero;
+            Costo = costo;
+            NombreCliente = nombreCliente;
+            NumeroCamas = numeroCamas;
+            Disponibilidad = disponibilidad;
+        }
+
+        public void MostrarInformacion(int numero, double costo, string nombreCliente, int numeroCamas, bool disponibilidad)
+        {
+            Console.WriteLine(numero);
+            Console.WriteLine(costo);
+            Console.WriteLine(nombreCliente);
+            Console.WriteLine(numeroCamas);
+            Console.WriteLine(disponibilidad);
+        }
+
+        public bool CambiarDisponibilidad(bool disponibilidad)
+        {
+            return true;
+        }
+
+        public void AsignarCliente()
+        {
+
+        }
     }
 
+    public class Suite : Habitacion 
+    {
+        public bool Jacuzzi { get; set; }
+        public int CantidadHabitaciones { get; set; }
+    }
+
+    public class HabitacionDeluxe : Suite
+    {
+        public bool Comidas { get; set; }
+    }
 }
